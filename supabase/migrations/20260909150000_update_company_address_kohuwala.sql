262829fa8f070a2214263ff2f9f0968da6ce3b15
thennakoontoursnew <thennakoontoursnew@gmail.com>
2026-09-09 13:53:10 +0530

    fix(migration): remove non-existent reg_number column from company_settings update

--- supabase/migrations/20260909150000_update_company_address_kohuwala.sql
+++ supabase/migrations/20260909150000_update_company_address_kohuwala.sql
@@ -1,5 +1,5 @@
 -- Migration: 20260909150000_update_company_address_kohuwala.sql
--- Purpose: Update company settings default address to Kohuwala
+-- Purpose: Update company settings default address and contact details to Kohuwala
 
 UPDATE company_settings
 SET address = 'No. 146/5A, Dutugemunu Street, Caldera Gardens, Kohuwala.',
@@ -8,5 +8,4 @@ SET address = 'No. 146/5A, Dutugemunu Street, Caldera Gardens, Kohuwala.',
     phone_primary = '+94 112 823 723',
     phone_secondary = '+94 777 273 820',
     whatsapp_number = '+94 777 474 938',
-    reg_number = 'PV00312253',
     default_agreement_location = 'Kohuwala, Sri Lanka';
